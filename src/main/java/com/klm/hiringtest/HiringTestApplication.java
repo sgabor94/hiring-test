341db33022805df64d94627e4e0169ef182c3bb1
Gabor Somogyi <gabor.somogyi@atolldev.hu>
2023-04-10 09:03:58 +0200

    Initial commit

---
+++ src/main/java/com/klm/hiringtest/HiringTestApplication.java
@@ -0,0 +1,13 @@
+package com.klm.hiringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klm/hiringtest/HiringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klm.hiringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
